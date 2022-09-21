9c6891473599d6eee32eb2c722258d8dc215f650
Natalie Rust <10840498@my.uvu.edu>
2022-09-21 14:49:18 -0600

    Practicing Arrays.cs

---
+++ Unit1c/Arrays.cs
@@ -0,0 +1,11 @@
+using System;
+					
+public class Program
+{
+	public void Main()
+	{
+		string[] animals = {"cat", "dog", "pig", "snake"};
+		Console.WriteLine(animals.Length);
+		Console.WriteLine(animals[2]);
+	}
+}
