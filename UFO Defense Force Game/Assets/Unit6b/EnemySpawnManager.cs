cda98ee2bda3a6051d17e2070c10338481726c41
Natalie <10840498@my.uvu.edu>
2022-11-04 23:36:34 -0600

    Unit 6b Lab Finish

--- UFO Defense Force Game/Assets/Unit6b/EnemySpawnManager.cs
+++ UFO Defense Force Game/Assets/Unit6b/EnemySpawnManager.cs
@@ -9,15 +9,19 @@ public class EnemySpawnManager : MonoBehaviour
   public GameObject[] ufoPrefabs;
   private float spawnRangex = 20f;
   private float spawnRangez = 20f;
-  
+  private float startDelay = 2f;
+  private float spawnInterval = 1.5f;
 
-  private void Update()
+  private void Start()
   {
-      if (Input.GetKeyDown(KeyCode.S))
-      {
-          Vector3 spawnPos = new Vector3(Random.Range(-spawnRangex, spawnRangex), 0, spawnRangez);
-          int ufoIndex = Random.Range(0, ufoPrefabs.Length);
-          Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation);
-      }
+      InvokeRepeating("SpawnRandomUFO", startDelay, spawnInterval);
   }
+
+  void SpawnRandomUFO()
+  {
+    Vector3 spawnPos = new Vector3(Random.Range(-spawnRangex, spawnRangex), 3, spawnRangez);
+    int ufoIndex = Random.Range(0, ufoPrefabs.Length);
+    Instantiate(ufoPrefabs[ufoIndex], spawnPos, ufoPrefabs[ufoIndex].transform.rotation);
+  }
+ 
 }
