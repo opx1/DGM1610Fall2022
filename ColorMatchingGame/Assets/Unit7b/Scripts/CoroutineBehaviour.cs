7152dd56f9b794e019aef041306c068393bec332
Natalie <10840498@my.uvu.edu>
2022-12-04 22:02:08 -0700

    Finishing 7c Challenge

--- ColorMatchingGame/Assets/Unit7b/Scripts/CoroutineBehaviour.cs
+++ ColorMatchingGame/Assets/Unit7b/Scripts/CoroutineBehaviour.cs
@@ -57,8 +57,8 @@ public class CoroutineBehaviour : MonoBehaviour
     {
         while (CanRun)
         {
-            yield return wfscObj;
             repeatUntilFalseEvent.Invoke();
+            yield return wfscObj;
         }
     }
 }
